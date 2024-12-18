94e4b8e7d9a8e1a75c3b09d7bbd1825d8df43304
ryanhung044 <hunglsb2004@gmail.com>
2024-12-18 11:15:34 +0700

    new version

--- app/Http/Controllers/Admin/ClassroomController.php
+++ app/Http/Controllers/Admin/ClassroomController.php
@@ -67,7 +67,7 @@ class ClassroomController extends Controller
             $orderDirection = $request->input('orderDirection', 'asc');
     
             $classrooms = Classroom::select(['classrooms.class_code', 'classrooms.class_name', 'classrooms.user_code', 'classrooms.subject_code', 'classrooms.is_active'])
-            ->where('classrooms.is_active',true)
+            // ->where('classrooms.is_active',true)
 
                 ->when($search, function($query) use ($search) {
                     $query->where('classrooms.class_code', "LIKE", "%$search%")
